3fc8f277bd1ae837dc67cd4e63ed18276baa6648
Jinnie-J <jjesweet@naver.com>
2025-07-18 13:47:18 +0900

    feat: 포인트 엔티티 추가

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/point/PointEntity.java
@@ -0,0 +1,48 @@
+package com.loopers.domain.point;
+
+import com.loopers.domain.user.UserEntity;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "point_entity")
+@Getter
+@NoArgsConstructor
+public class PointEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int balance;
+
+    private String userId;
+
+    public PointEntity(int balance, String userId) {
+        if(balance < 0){
+            throw new CoreException(ErrorType.BAD_REQUEST);
+        }
+        this.balance = balance;
+        this.userId = userId;
+    }
+
+    public void increase(int amount){
+        if(amount <= 0){
+            throw new CoreException(ErrorType.BAD_REQUEST);
+        }
+        int newBalance = this.balance + amount;
+        this.balance = newBalance;
+    }
+
+    public void decrease(int amount){
+        if(amount <= 0){
+            throw new CoreException(ErrorType.BAD_REQUEST);
+        }
+        int newBalance = this.balance - amount;
+        this.balance = newBalance;
+    }
+
+}
