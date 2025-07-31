330eb44e611b331929ea040e673d08a14c1260ec
Jinnie-J <jjesweet@naver.com>
2025-07-31 22:18:11 +0900

    feat: 좋아요 엔티티 추가

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/like/LikeEntity.java
@@ -0,0 +1,31 @@
+package com.loopers.domain.like;
+
+import com.loopers.domain.BaseEntity;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "likes")
+@Getter
+@NoArgsConstructor
+public class LikeEntity extends BaseEntity {
+
+    private Long userId;
+    private Long productId;
+
+    private LikeEntity(Long userId, Long productId) {
+        if(userId == null || productId == null) {
+            throw new CoreException(ErrorType.BAD_REQUEST);
+        }
+        this.userId = userId;
+        this.productId = productId;
+    }
+
+    public static LikeEntity of(Long userId, Long productId) {
+        return new LikeEntity(userId, productId);
+    }
+}
