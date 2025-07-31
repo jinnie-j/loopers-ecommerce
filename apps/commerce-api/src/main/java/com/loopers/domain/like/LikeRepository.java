cf59dd0d5be0d91e1626a3942ca4c93d253b14f1
Jinnie-J <jjesweet@naver.com>
2025-07-31 22:19:29 +0900

    feat: 좋아요 Repository 구현

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/like/LikeRepository.java
@@ -0,0 +1,17 @@
+package com.loopers.domain.like;
+
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LikeRepository {
+    boolean exists(Long userId, Long productId);
+
+    LikeEntity save(LikeEntity likeEntity);
+
+    Optional<LikeEntity> find(long userId, long productId);
+
+    void deleteByUserIdAndProductId(long userId, long productId);
+
+    List<LikeEntity> findByUserId(long userId);
+}
---
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/like/LikeJpaRepository.java
@@ -0,0 +1,17 @@
+package com.loopers.infrastructure.like;
+
+import com.loopers.domain.like.LikeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LikeJpaRepository extends JpaRepository<LikeEntity, Long> {
+    boolean existsByUserIdAndProductId(Long userId, Long productId);
+
+    Optional<LikeEntity> findByUserIdAndProductId(long userId, long productId);
+
+    void deleteByIdAndProductId(long userId, long productId);
+
+    List<LikeEntity> findByUserId(long userId);
+}
