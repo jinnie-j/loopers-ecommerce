dd987cd4e710e6b1567df27e85cd0fbcd95bcc43
Jinnie-J <jjesweet@naver.com>
2025-07-31 22:20:58 +0900

    feat: 주문 조회, 생성 기능 구현

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/order/OrderService.java
@@ -0,0 +1,39 @@
+package com.loopers.domain.order;
+
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Component
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public OrderInfo createOrder(OrderCommand.Order orderCommand) {
+        List<OrderItemEntity> items = orderCommand.orderItems().stream()
+                .map(item -> OrderItemEntity.of(item.productId(), item.quantity(), item.price()))
+                .toList();
+        OrderEntity orderEntity = OrderEntity.of(orderCommand.userId(), items);
+        OrderEntity saved = orderRepository.save(orderEntity);
+        return OrderInfo.from(saved);
+    }
+
+    public List<OrderInfo> getOrders(Long userId) {
+        List<OrderEntity> orderEntities = orderRepository.findByUserId(userId);
+
+        return orderEntities.stream().map(OrderInfo::from).collect(Collectors.toList());
+
+    }
+
+    public OrderInfo getOrder(Long orderId) {
+        OrderEntity orderEntity = orderRepository.findById(orderId)
+                .orElseThrow(() -> new CoreException(ErrorType.NOT_FOUND));
+
+        return OrderInfo.from(orderEntity);
+    }
+}
