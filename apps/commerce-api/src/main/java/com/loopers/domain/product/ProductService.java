94ad8829f7e4ddb629692f15782dbf4811a969d5
Jinnie-J <jjesweet@naver.com>
2025-08-06 00:59:39 +0900

    feat: Product interfaces 레이어 구현, E2E 테스트 작성

--- apps/commerce-api/src/main/java/com/loopers/domain/product/ProductService.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/product/ProductService.java
@@ -1,14 +1,20 @@
 package com.loopers.domain.product;
 
+import com.loopers.application.product.ProductQueryRepository;
 import com.loopers.support.error.CoreException;
 import com.loopers.support.error.ErrorType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
+    private final ProductQueryRepository productQueryRepository;
 
     public ProductInfo create(ProductCommand.Create productCommand) {
         ProductEntity productEntity = ProductEntity.of(
@@ -24,6 +30,14 @@ public class ProductService {
         ProductEntity productEntity = productRepository.findById(productId)
                 .orElseThrow(()-> new CoreException(ErrorType.NOT_FOUND));
         return ProductInfo.from(productEntity);
+    }
+
+    public List<ProductInfo> getProducts(ProductSortType sort) {
+        Pageable pageable = PageRequest.of(0, 100);
+        List<ProductEntity> products = productQueryRepository.findBySortType(sort, pageable);
+        return products.stream()
+                .map(ProductInfo::from)
+                .toList();
     }
 
     public void decreaseStock(Long productId, Long quantity) {
